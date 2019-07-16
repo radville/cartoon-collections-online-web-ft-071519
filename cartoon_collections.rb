704a0b76a2801b1a40f8364c2e45a65a998e60d1
Laura Radville <laura.radville@gmail.com>
2019-07-16 14:05:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.include?(cheese_types)
 end
