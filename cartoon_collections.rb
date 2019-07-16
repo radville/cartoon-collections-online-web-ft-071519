f7038371d4ab8495ec6005c771711f769e242f83
Laura Radville <laura.radville@gmail.com>
2019-07-16 14:03:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def summon_captain_planet(array)
   new
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do 
 end
 
 def find_the_cheese# code an argument here
